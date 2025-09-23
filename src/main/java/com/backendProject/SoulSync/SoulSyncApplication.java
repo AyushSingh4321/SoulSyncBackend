3a4839475b6ef089701eb7756f4010d0e96019ee
AyushSingh4321 <ayushksb11@gmail.com>
2025-09-24 03:18:39 +0530

    fix: deployment issue 1

--- src/main/java/com/backendProject/SoulSync/SoulSyncApplication.java
+++ src/main/java/com/backendProject/SoulSync/SoulSyncApplication.java
@@ -9,11 +9,11 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class SoulSyncApplication {
 
 	public static void main(String[] args) {
-		  Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry ->
-            System.setProperty(entry.getKey(), entry.getValue())
-        );
-		System.out.println("SPRING_MAIL_HOST from env = " + System.getProperty("SPRING_MAIL_HOST"));
+//		  Dotenv dotenv = Dotenv.load();
+//        dotenv.entries().forEach(entry ->
+//            System.setProperty(entry.getKey(), entry.getValue())
+//        );
+//		System.out.println("SPRING_MAIL_HOST from env = " + System.getProperty("SPRING_MAIL_HOST"));
 		SpringApplication.run(SoulSyncApplication.class, args);
 		System.out.println("Running");
 	}
