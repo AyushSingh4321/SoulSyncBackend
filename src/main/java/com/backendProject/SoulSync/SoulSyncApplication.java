55d97b9347dbafc636df90cdb5c36caa5e2cc783
AyushSingh4321 <ayushksb11@gmail.com>
2025-09-24 03:08:59 +0530

    fix: dotenv loading issue

--- src/main/java/com/backendProject/SoulSync/SoulSyncApplication.java
+++ src/main/java/com/backendProject/SoulSync/SoulSyncApplication.java
@@ -3,10 +3,17 @@ package com.backendProject.SoulSync;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class SoulSyncApplication {
 
 	public static void main(String[] args) {
+		  Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
+		System.out.println("SPRING_MAIL_HOST from env = " + System.getProperty("SPRING_MAIL_HOST"));
 		SpringApplication.run(SoulSyncApplication.class, args);
 		System.out.println("Running");
 	}
